85d111505dec20add43a23fda362d20088d66095
Ragavan Natarajan <n.ragav@gmail.com>
2017-03-11 22:14:42 +0530

    Added Buggy Date Parser
    Added code to demonstrate the pitfalls of hiding in OO

---
+++ ooyala/introduction/java/hiding/buid.sbt
@@ -0,0 +1,7 @@
+name := "OO hiding"
+
+version := "1.0"
+
+assemblyJarName in assembly := "Hiding.jar"
+
+mainClass in assembly := Some("online.ragavan.hiding.Hiding")
---
+++ ooyala/introduction/java/hiding/src/main/java/online/ragavan/hiding/Hiding.java
@@ -0,0 +1,61 @@
+package online.ragavan.hiding;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Random;
+
+class Hiding {
+
+    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+    public Date parseDate(String date) {
+        try {
+            return df.parse(date);
+        } catch (Exception exc) {
+            System.out.println("Input date: " + date + 
+                    "\nMessage: " + exc.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] argv) {
+
+        Hiding hiding = new Hiding();
+        Random rand = new Random();
+
+        /*
+         * Create and start 1000 threads to simulate a real world concurrent
+         * application.
+         */
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = new Thread(new Runnable() { 
+                public void run() {
+                    /*
+                     * Introduce a random delay.
+                     */
+                    try { 
+                        Thread.sleep(rand.nextInt(5));
+                    } catch (InterruptedException exc) { 
+                        exc.printStackTrace(); 
+                    }
+                    
+                    /*
+                     * Creating some random but valid Year, Month and Day for
+                     * input.
+                     */
+                    int year = rand.nextInt(50) + 2000;
+                    int month = rand.nextInt(12) + 1;
+                    int day = rand.nextInt(27) + 1;
+                    String date = year + "-" + month + "-" + day;
+                    System.out.println("Input : " + date + 
+                            "; Output: " + hiding.parseDate(date));
+                }
+            });
+            thread.start();
+        }
+    }
+
+}
